f5e4e69178d259219df4dedc8ec72842fa9fb783
Dung <hovandung9778@gmail.com>
2025-11-09 17:55:33 +0700

    layout tuần 4

---
+++ project/example-app/routes/web.php
@@ -0,0 +1,32 @@
+<?php
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('index');
+})->name('index');
+Route::get('/shop', function () {
+    return view('shop');
+})->name('shop');
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+Route::get('/blog', function () {
+    return view('blog');
+})->name('blog');
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+Route::get('/cart', function () {
+    return view('cart');
+})->name('cart');
+Route::get('/wishlist', function () {
+    return view('wishlist');
+})->name('wishlist');
+Route::get('/product-single', function () {
+    return view('product-single');
+})->name('product-single');
+Route::get('checkout', function () {
+    return view('checkout');
+})->name('checkout');
+
+
